c62d42801b440b199effd506a8b128e60d066d4f
Meeran Husain <meeranhusain3@gmail.com>
2024-10-31 03:18:20 +0530

    code with harry Js

---
+++ JavaScript/33_Siblings_And_Parents/33script.js
@@ -0,0 +1,7 @@
+// https://replit.com/@codewithharry/33SiblingsAndParents
+
+console.log(document.body.firstChild)
+a = document.body.firstChild
+console.log(a.parentNode)
+console.log(a.parentElement)
+console.log(a.firstChild.nextSibling)
