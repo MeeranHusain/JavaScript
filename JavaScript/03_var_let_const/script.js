cead4d06609691a3a734f78c704c21c0b28494fe
Meeran Husain <meeranhusain3@gmail.com>
2024-11-04 16:16:26 +0530

    var, let and const CWH Js

---
+++ JavaScript/03_var_let_const/script.js
@@ -0,0 +1,14 @@
+console.log("JavaScript tut 3: var, let and const");
+// var a = 45;
+// var a = "p";
+let b = "Meeran";
+let c = null;
+let d = undefined;
+// const author = "Meeran";
+author  = 5; // Throws an error because constant cannot be changed
+{
+  let b = "this";
+  console.log(b);
+}
+
+console.log(b);
