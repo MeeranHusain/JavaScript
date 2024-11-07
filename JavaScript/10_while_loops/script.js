97402696e5b657951b73958d881678a7b6c856a8
Meeran Husain <meeranhusain3@gmail.com>
2024-11-07 21:38:29 +0530

    while loops CWH Js

---
+++ JavaScript/10_while_loops/script.js
@@ -0,0 +1,21 @@
+const prompt = require("prompt-sync")({ signit: true });
+// while loop
+/*
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+
+let i = 0;
+while(i<n){
+  console.log(i)
+  i++;
+}
+*/
+// while loop
+let n = prompt("Enter the value of n ")
+n = Number.parseInt(n)
+
+let i = 10;
+do{
+  console.log(i)
+  i++;
+}while (i < n) 
